316dac53cd314dee3d352fa0d633d0971b7f02e7
Ritaobuke <obukerita23@gmail.com>
2022-10-17 04:47:01 -0700

    my second if_else statement

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive, zero, or negative
+ * Discription: using the min function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int R;
+	char str[] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	R = n % 12;
+	if (R > 5)
+		printf("%s %d is %d and is greater than 5\n", str, n, R);
+	else if (R == 0)
+		printf("%s %d is %d and is 0\n", str, n, R);
+	else if (R < 6)
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, R);	return (0);
+}
+
