c397ce6f106c084e6e552a6b1d20076ba92d9984
Ritaobuke <obukerita23@gmail.com>
2022-10-18 15:34:13 -0700

    prin alphabet 10 times in lower case followed by new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * main - prints 10 times the alphabet, in lowercase.
  * Description: code by Rukevwe
  */
 
