56a3603b674f3eb819e11e949c16b316d156be6e
Ritaobuke <obukerita23@gmail.com>
2022-10-20 09:36:13 -0700

    header file for dubugging task

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * File - main.h
+ * Auth - Rukevwe
+ * Description - heager file used for 0x03-debugging
+ */
+
+void positive_or_negative(int i);
+int infinite_loop(int i);
+int largest_number(int a, int b, int c);
+void convert_date (int day, int month, int year);
+
+#endif
+
