0683ae886ff8fb040b7140a2e05b2bb9d45656be
Ritaobuke <obukerita23@gmail.com>
2022-10-20 10:19:43 -0700

    header for 0x03-debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int convert_day(int month, int day);
+int comment_out(int c);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-
+void convert_date(int day, int month, int year);
 
 #endif
+
