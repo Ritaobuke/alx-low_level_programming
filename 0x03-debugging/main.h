38f22dd83f3b51b246f17515c554d744ddc8a856
Ritaobuke <obukerita23@gmail.com>
2022-10-21 03:15:04 -0700

    header file for 0x03-debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(i);
+int comment_out(int i);
+int largest_number(int a, int b, int c);
+void remainin_day(int day, int month, int year);
+
+#endif
+
