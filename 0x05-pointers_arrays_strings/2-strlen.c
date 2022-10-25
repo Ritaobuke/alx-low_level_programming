b01a05f2d34a53bdfd0426f6ebdc185c79f9c131
Ritaobuke <obukerita23@gmail.com>
2022-10-24 21:28:51 -0700

    return the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,9 @@
 
 /**
  * strlen - return the length of a string
- * Description: _strlen will return the length of a string
- * @s: char to check
  * 
+ * @s: char to check
+ * Description: _strlen will return the length of a string
  * Return: 0 is success
  */
 
