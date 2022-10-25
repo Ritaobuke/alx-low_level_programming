aa532b9b9c04249c07af60eea6839caa71b98038
Ritaobuke <obukerita23@gmail.com>
2022-10-24 20:46:26 -0700

    function that prints the length iof a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _strlen - return the length of a string
- * 
+ * int _strlen - return the length of a string
+ *
  * @s: char to check
- * Description: this will return the length of a string
  * Return: 0 is success
  */
 
