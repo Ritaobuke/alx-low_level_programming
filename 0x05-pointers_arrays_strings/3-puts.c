de6c1447d3a7afa1bd002412e624527555352eb5
Ritaobuke <obukerita23@gmail.com>
2022-10-24 21:06:47 -0700

    prints the legth of the string in new line to stdio

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 /**
  *  puts - prints a string
  *  @str: the string
- *  Description: prints the lenth of the string to standars output
+ *  Description: _put to print the lenth of the string to standars output
  *
  *  Return: the length of the string
  */
