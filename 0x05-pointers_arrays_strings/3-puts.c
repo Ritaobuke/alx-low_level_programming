e52c8ad2418f80be798d226c9ae96cc492c7aa5f
Ritaobuke <obukerita23@gmail.com>
2022-10-24 20:57:26 -0700

    function that prints thr length of a string and a new line in sdio

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
+ *  puts - prints a string
  *  @str: the string
  *
  *  Return: the length of the string
@@ -13,9 +13,9 @@ void _puts(char *str)
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
 
